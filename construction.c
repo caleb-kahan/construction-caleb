9bd9b107a4ab3fbcc9a18cc468e514354d4bf712
Caleb Kahan <ckahan00@stuy.edu>
2019-10-16 15:37:57 -0400

    works

--- construction.c
+++ construction.c
@@ -1,2 +1,32 @@
 #include <stdio.h>
-struct person {int IQ; char *s "Zodiac Sign";};
+#include <stdlib.h>
+#include <time.h>
+
+struct person {int IQ; char *ZodiacSign;};
+
+struct person exampleProducer(){
+  srand(time(NULL));
+  char *signs[12] = {"Aries", "Taurus", "Gemini", "Cancer", "Leo", "Virgo", "Libra", "Scorpio", "Sagittarius", "Capricorn", "Aquarius" , "Pisces"};
+  int iqs[12];
+  int i;
+  for(i=0;i<12;i++){
+    int random = rand();
+    if (random%2)
+      random*=-1;
+    iqs[i] = 100 + random%50;
+  }
+  int random2 = rand()%12;
+
+  struct person caleb;
+  caleb.IQ = iqs[random2];
+  caleb.ZodiacSign = signs[random2];
+  return caleb;
+}
+void structPrinter(struct person caleb){
+  printf("Person's IQ: %d, Person's Zodiac Sign: %s\n", caleb.IQ, caleb.ZodiacSign);
+}
+int main(){
+  struct person caleb = exampleProducer();
+  structPrinter(caleb);
+  return 0;
+}
